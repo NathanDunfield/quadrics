c4a7aeea26bd13ed96c7cd0d2b1deced9ae85e04
Nathan Dunfield <nathan@dunfield.info>
2016-08-11 12:11:22 -0500

    Down with dat.gui, up with noUi!

--- js/basicsphere.js
+++ js/basicsphere.js
@@ -1,65 +1,65 @@
 /* Wrapped in an anonymous function call so all variables are local to this file. */
 (function () {
+    var gui, scene;
+    var parameters;
+    var sphere;
+    var edges;
+    var slider;
+    var sliderLabel;
+    
+    init();
 
-var gui, scene;
-var parameters;
-var sphere;
-var edges;
-var slider;     
+    function init()
+    {
+	var values = setup3DScene("basicsphere");
+	scene = values.scene;
+	var animate = values.animate;
 
-init();
+	slider = document.getElementById("radiusslider");
+	sliderLabel = document.getElementById("radiussliderlabel");
+	noUiSlider.create(slider, {
+	    start: 2.0,
+	    range: {"min": 0.5, "max": 3.0},
+	    orientation: "horizontal",
+	    connect: "lower",
+	});
 
-function init()
-{
-    var values = setup3DScene("basicsphere");
-    gui = values.gui;
-    scene = values.scene;
-    var animate = values.animate;
+	slider.noUiSlider.on("update", updateSphere)
+	// parameters = {radius:2.0};
+	// gui.add(parameters, 'radius', 1, 3, 0.1).onChange(updateSphere);
 
-    slider = document.getElementById("radiusslider");
-    noUiSlider.create(slider, {
-	start: 2.0,
-	range: {"min": 0.5, "max": 3.0},
-	tooltips: true,
-	orientation: "horizontal",
-	connect: "lower",
-    });
+	fancyLighting(scene);
 
-    slider.noUiSlider.on("update", updateSphere)
-    // parameters = {radius:2.0};
-    // gui.add(parameters, 'radius', 1, 3, 0.1).onChange(updateSphere);
-
-    fancyLighting(scene);
-
-    var ticks = [-2, 0, 2];
-    scene.add(axes(3, ticks, 3, ticks, 3, ticks, 0.2, 0.4, 0.8));
-    slider.noUiSlider.set(2.0);
-    animate();
-}
+	var ticks = [-2, 0, 2];
+	scene.add(axes(3, ticks, 3, ticks, 3, ticks, 0.2, 0.4, 0.8));
+	slider.noUiSlider.set(2.0);
+	animate();
+    }
 
 
-function updateSphere()
-{
-    scene.remove(sphere);
-    scene.remove(edges);
-    drawSphere();
-}
-
-function drawSphere()
-{
-    var radius = slider.noUiSlider.get();
-    console.log(radius);
-    // Sphere parameters: radius, segments along width, segments along height
-    var geometry = new THREE.SphereGeometry(radius, 32, 16 );
-    geometry.rotateX(Math.PI/2);
-    // use a "lambert" material rather than "basic" for realistic lighting.
-    var material = new THREE.MeshLambertMaterial({color:0xEEEEEE });
-    sphere = new THREE.Mesh(geometry, material);
-    sphere.position.set(0, 0, 0);
-    scene.add(sphere);
-    edges = new THREE.EdgesHelper(sphere, "black");
-    edges.material.linewidth=1.5;
-    scene.add(edges);
+    function updateSphere()
+    {
+	scene.remove(sphere);
+	scene.remove(edges);
+	drawSphere();
+    }
+    
+    function drawSphere()
+    {
+	var radius = Number(slider.noUiSlider.get());
+	// Set the label
+	sliderLabel.innerHTML = "radius = " + radius.toFixed(1);
+	// Sphere parameters: radius, segments along width, segments along height
+	var geometry = new THREE.SphereGeometry(radius, 32, 16 );
+	geometry.rotateX(Math.PI/2);
+	// use a "lambert" material rather than "basic" for realistic lighting.
+	var material = new THREE.MeshLambertMaterial({color:0xEEEEEE });
+	sphere = new THREE.Mesh(geometry, material);
+	sphere.position.set(0, 0, 0);
+	scene.add(sphere);
+	edges = new THREE.EdgesHelper(sphere, "black");
+	edges.material.linewidth=1.5;
+	scene.add(edges);
 }
 
 }()); // calling anonymous function. 
